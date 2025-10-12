7c8677d1860f88158bc2167ba2a670b5c00a9af0
AnhSonNguyen <anhsonnguyen1806@gmail.com>
2025-10-12 17:06:37 +0700

    Giao diện quản lý thực đơn

---
+++ Areas/Admin/Controllers/MenuManagerController.cs
@@ -0,0 +1,24 @@
+﻿using LibraryManagerment.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagerment.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuManagerController : Controller
+    {
+        private readonly QltvthttContext _context;
+        public MenuManagerController(QltvthttContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var menus = await _context.TbMenus.ToListAsync();
+            return View(menus);
+        }
+
+        // Thêm các action Create, Edit, Delete ở đây (scaffold hoặc tự viết)
+    }
+}
