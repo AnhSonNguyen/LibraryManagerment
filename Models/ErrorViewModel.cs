768865a30fac8ce54bb60dfa0c0b3a9a0c8a9cff
AnhSonNguyen <anhsonnguyen1806@gmail.com>
2025-10-12 10:11:49 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryManagerment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ViewComponents/MenuTopViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using LibraryManagerment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagerment.ViewComponents
+{
+    public class MenuTopViewComponent : ViewComponent
+    {
+        private readonly QltvthttContext _context;
+        public MenuTopViewComponent(QltvthttContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.TbMenus.Where(m => (bool)m.IsActive).
+                OrderBy(m => m.Position).ToList();
+            return await Task.FromResult<IViewComponentResult>(View(items));
+        }
+    }
+}
