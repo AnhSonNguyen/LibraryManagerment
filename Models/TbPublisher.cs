2255f17e4aff967a6cfb0c449c0d762df3f2faf0
nhanjelly <thiennhan5a2@gmail.com>
2025-11-02 15:20:50 +0700

    model nxb

---
+++ Models/TbPublisher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LibraryManagerment.Models
+{
+    public partial class TbPublisher
+    {
+        public int PublisherId { get; set; }
+        public string? PublisherName { get; set; }
+        public string? Description { get; set; }
+    }
+}
